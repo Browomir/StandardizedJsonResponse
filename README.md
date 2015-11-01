2ac18490b070b574770add8b7ed2ca99513e751c
Browomir <tomczak.sebastian.st@gmail.com>
2015-11-01 18:07:49 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# StandardizedJsonResponse
+StandardizedJsonResponse
+=========
 Simple class to prepare standardized JSON response based on JSend specification
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require browomir/StandardizedJsonResponse "dev-master"
+```
+
+or add
+
+```
+"browomir/StandardizedJsonResponse": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+### Migration
+
+Migration run
+
+```php
+        $response = new StandardizedJsonResponse();
+        $response->setStatus(StandardizedJsonResponse::STATUS_SUCCESS);
+        $response->setData(
+            array(
+                'lorem' => 'ipsum'
+            )
+        );
+        $response->setMessage('The message');
+        return $response->getResponse();
+```
---
+++ src/StandardizedJsonResponse.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Helper;
+
+
+class StandardizedJsonResponse
+{
+    const STATUS_SUCCESS = 'success';
+    const STATUS_ERROR = 'error';
+    const STATUS_FAIL = 'fail';
+    const DEFAULT_MESSAGE = 'Something went wrong!';
+
+    private $status;
+    private $message;
+    private $data;
+
+    public function __construct($status = self::STATUS_ERROR, $message = self::DEFAULT_MESSAGE, $data = null)
+    {
+        $this->setStatus($status);
+        $this->setMessage($message);
+        $this->setData($data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * return array - it can be easily serialized
+     * @return array
+     */
+    public function getResponse()
+    {
+        return array(
+            'status' => $this->status,
+            'data' => $this->data,
+            'message' => $this->message
+        );
+    }
+}
